f6f3e91969e3a3550d2abad217126f40a155e591
kirsty mott <kirstymallon@hotmail.com>
2023-11-21 12:35:19 +0000

    updated by adding; and caps

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,7 +12,7 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
-        system.out.println(sayHellotosomeone("Kirsty"))
+        system.out.println(sayHelloToSomeone("Kirsty"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
