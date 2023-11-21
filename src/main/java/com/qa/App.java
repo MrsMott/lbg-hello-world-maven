9d360db18073bdd101e44cf096fb7dc0c35811df
kirsty mott <kirstymallon@hotmail.com>
2023-11-21 13:30:57 +0000

    Added another hello

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Kirsty"));
         System.out.println(sayHelloToSomeone("Anyone"));
+        System.out.println(sayHelloToSomeone("Someone"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
